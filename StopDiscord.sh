1b4d74e6dc701416334e4337e65aa3af68a99488
Spencer Burke <info.spencer.burke@gmail.com>
2021-02-26 20:40:01 -0800

    Making sure the script won't produce error messages.

--- StopDiscord.sh
+++ StopDiscord.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ps ax | grep "/app/discord/Discord" | head -n 1 | sed "s/[?].*//" | xargs kill
+# This script will kill the running discord process
+ps ax | grep "/app/discord/Discord" | head -n 1 | sed "s/[?].*//" | xargs kill 2&> /dev/null
 
